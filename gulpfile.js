03ed01f66bbd9d751a542400f6e558832a17bed9
Nikita <nikitaloginov264@gmail.com>
2025-12-05 08:16:03 +0300

    старотовая архитектура

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const fileInclude = require('gulp-file-include');
+const browserSync = require('browser-sync').create();
+
+gulp.task('html', function () {
+    return gulp.src('html/pages/*.html')
+        .pipe(fileInclude({
+            prefix: '@@', 
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('.')) 
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function () {
+    browserSync.init({
+        server: './' 
+    });
+
+    gulp.watch('html/**/*.html', gulp.series('html'));
+    gulp.watch('css/*.css').on('change', browserSync.reload);
+    gulp.watch('html/modules/*.html').on('change', browserSync.reload); 
+});
+
+gulp.task('default', gulp.series('html', 'watch'));
