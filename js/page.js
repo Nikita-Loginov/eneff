dbbd2249c01c45976a2e7de716f8a0c509b6ef12
Nikita <nikitaloginov264@gmail.com>
2025-12-15 14:37:44 +0300

    ui

--- js/page.js
+++ js/page.js
@@ -1,12 +1,24 @@
 import { getHeightHeader, checkScrollY } from "./modules/header.js";
 import { searcBlockTab } from "./modules/tab.js";
+import { initModal, checkStartOpen } from "./modules/modal.js";
+import { initFormValidation } from "./modules/validate.js";
 
 const handleGlobalClick = (e) => {
   searcBlockTab(e)
+  initModal(e);
+};
+
+const initValidate = () => {
+  const forms = document.querySelectorAll("form.form");
+  forms.forEach((form) => {
+    initFormValidation(form);
+  });
 };
 
 document.addEventListener("DOMContentLoaded", () => {
+  checkStartOpen();
   getHeightHeader();
+  initValidate();
 
   document.addEventListener("click", handleGlobalClick);
 });
